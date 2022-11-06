dbc5cc06218fd1d99f698bcbbf66e5f2100f01ef
Adan Montiel Valdez <adan.montiel@gmail.com>
2022-11-06 22:59:33 +0100

    componenteTeste

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Test from './components/Test'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Aplicação de Exemplo!!!</p>
+        <Test/>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
