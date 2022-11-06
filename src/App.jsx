e7163692ed1b117548ab3dbcec779ae2c4e4bd71
Adan Montiel Valdez <adan.montiel@gmail.com>
2022-11-06 22:57:41 +0100

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import Test from './components/Test'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-  <Test/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Aplicação de Exemplo!!!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
     </div>
   )
 }
